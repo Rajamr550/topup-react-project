802fec50e538f2811d9da2a3c791e153e13ac8b8
raja <raja.m@zensar.com>
2022-09-10 22:32:17 +0530

    added table data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import DataFetching from "./getApiData";
+// import A from "./A";
+
+import JsonDataDisplay from "./json";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <JsonDataDisplay />
     </div>
-  );
+  )
 }
 
 export default App;
