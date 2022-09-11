2f2bbaa5f10fc82dcc62f8e2db2140b8dfeb9fbf
raja <raja.m@zensar.com>
2022-09-11 10:35:43 +0530

    navigate

--- src/json.js
+++ src/json.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Table } from 'react-bootstrap'
 import Button from 'react-bootstrap/Button';
+import { Link } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
 function JsonDataDisplay() {
     const datas = [{ "id": 1, "plan": "truly unlimited", "details": "12 GB data, 100 free sms per day, unlimited outgoing calls", "amount": 499, "validity": "3 months" }, { "id": 2, "plan": "cricket packs", "details": "Disney + hotstar subscription for 3 months, 2GB data, outgoing calls at 1p per second", "amount": 785, "validity": "45 days" }, { "id": 3, "plan": "smart recharge", "details": "2GB data per day, outgoing calls at 60p per minute, 100 free sms per day", "amount": 325, "validity": "30 days" }, { "id": 4, "plan": "international roaming", "details": "100 mins of incoming/outgoung (India+ Local) day, countries supported: Nepal, Srilanka, Bhutan, Pakistan, Bangladesh", "amount": 899, "validity": "30 days" }];
 
+    const navigate = useNavigate();
     return (
         <div className="boot">
-            <h1>List With Bootstrap Table</h1>
+            <h1>TopUp Plans</h1>
             <Table bordered hover  >
                 <tbody>
                     <tr>
@@ -26,15 +29,26 @@ function JsonDataDisplay() {
                                 <td>{item.details}</td>
                                 <td>{item.amount}</td>
                                 <td>{item.validity}</td>
-                                <td><Button variant="primary">+</Button></td>
+                                <td><Button variant="primary" onClick={() => { navigate("/pay"); }}>+</Button></td>
 
                             </tr>
                         )
                     }
                 </tbody>
             </Table>
+
+            <Link to="/insert/your/path/here" className="btn btn-primary">hello</Link>
+            <Link to="/pay" className="btn btn-primary">pay</Link>
         </div>
     );
 }
+
+
+// function navigateToPay(item) {
+//     console.log(item);
+//     window.open("/pay");
+
+// }
+
 
 export default JsonDataDisplay;
