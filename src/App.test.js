d43066756241dbbaec65ac786c5823bd9ea2eeef
Rajamr550 <rajamr550@gmail.com>
2022-09-14 23:17:54 +0530

    app.js test completed

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,18 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 
-// test('renders App', () => {
-//   render(<App />);
-//   const linkElement = screen.getByTestId("app-test")
-//   expect(linkElement).toBeInTheDocument();
-//   //expect(linkElement).toHaveTextContent("Home");
+test('renders App', () => {
+  render(
+  <BrowserRouter>
+    <App />
 
-// });
+  </BrowserRouter>
+  );
+  const linkElement = screen.getByTestId("app-test")
+  expect(linkElement).toBeInTheDocument();
+  expect(linkElement).toHaveTextContent("Welcome to Vodafone Rechaarge App");
+
+  expect(linkElement).toHaveTextContent("Home");
+
+});
