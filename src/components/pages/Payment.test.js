6db060330b79775ca295daa352825c0361798348
Rajamr550 <rajamr550@gmail.com>
2022-09-15 00:03:37 +0530

    48 %

--- src/components/pages/Payment.test.js
+++ src/components/pages/Payment.test.js
@@ -1,18 +1,30 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import '@testing-library/jest-dom'
 import Payment from './Payment';
 
 
-test('renders Payment', () => {
-    render(<Payment />);
+test('renders pay tests', () => {
+    render(
+    <BrowserRouter>
+      <Payment />
+  
+    </BrowserRouter>
+    );
     const element = screen.getByTestId('payTest')
     expect(element).toBeInTheDocument();
-    expect(element).toHaveTextContent("Payment");
+    expect(element).toHaveTextContent("Payment Page");
+  //  expect(element).toHaveTextContent("Amount 785cricket packsDisney + hotstar subscription for 3 months, 2GB data, outgoing calls at 1p per second45 days");  
+    expect(element).toHaveTextContent("PAY");
+    expect(element).toHaveTextContent("Cancel"); 
+  });
 
-});
-
-test('renders Payment State', () => {
-    render(<Payment />);
-    const element = screen.getByTestId('payTestState')
+test('renders Payment States', () => {
+    render( <BrowserRouter>
+        <Payment />
+    
+      </BrowserRouter>);
+    const element = screen.getByTestId('amount')
     expect(element).toBeInTheDocument();
     expect(element).toHaveTextContent("Amount");
 
